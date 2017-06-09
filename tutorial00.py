b83f6600a7610311707845ad3b0e80f5755117ed
Kelvin <albertusk15@gmail.com>
2017-06-09 21:47:08 +0700

    add first exploration

---
+++ tutorial00.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+sess = tf.Session()
+
+W = tf.Variable([.3], tf.float32)
+b = tf.Variable([-.3], tf.float32)
+x = tf.placeholder(tf.float32)
+linear_model = W * x + b
+
+init = tf.global_variables_initializer()
+sess.run(init)
+
+print(sess.run(linear_model, {x:[1,2,3,4]}))
